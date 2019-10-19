4c4fb3d9620f43527ef1eff0d21b55ad9f8b5259
zhuliangcai <1024955966@qq.com>
2019-10-19 17:39:35 +0800

    finchley分利奇版本springcloud

--- springcloud-alibaba/nacos-config/nacos-provider/src/main/java/com/forezp/web/ConfigController.java
+++ springcloud-alibaba/nacos-config/nacos-provider/src/main/java/com/forezp/web/ConfigController.java
@@ -15,9 +15,18 @@ public class ConfigController {
 
     @Value("${username:lily}")
     private String username;
-
+    @Value("${username1:lily}")
+    private String username1;
+    @Value("${a:x}")
+    private String a;
+    @Value("${b:y}")
+    private String b;
     @RequestMapping("/username")
     public String get() {
+        System.out.println(username);
+        System.out.println(username1);
+        System.out.println(a);
+        System.out.println(b);
         return username;
     }
 }
